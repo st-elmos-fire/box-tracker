6ad3c790c0d011868eab81e1ab5df7c4ee62d0a7
Alex Foxleigh <alex@alexfoxleigh.com>
2022-08-05 22:35:34 +0100

    Created a bunch of icons - only needed tag for this ticket but got excited

--- components/atoms/icons/index.ts
+++ components/atoms/icons/index.ts
@@ -2,11 +2,35 @@ import React from 'react';
 import House from './library/house';
 import AppLogo from './library/app-logo';
 import Upload from './library/upload';
+import Add from './library/add';
+import Delete from './library/delete';
+import Edit from './library/edit';
+import Close from './library/close';
+import Refresh from './library/refresh';
+import Search from './library/search';
+import Arrow from './library/arrow';
+import Chevron from './library/chevron';
+import Error from './library/error';
+import Success from './library/success';
+import Warning from './library/warning';
+import Tag from './library/tag';
 
 interface ComponentProps extends React.FC<React.ComponentProps<'div'>> {
   House: typeof House;
   AppLogo: typeof AppLogo;
   Upload: typeof Upload;
+  Add: typeof Add;
+  Delete: typeof Delete;
+  Edit: typeof Edit;
+  Close: typeof Close;
+  Refresh: typeof Refresh;
+  Search: typeof Search;
+  Arrow: typeof Arrow;
+  Chevron: typeof Chevron;
+  Error: typeof Error;
+  Success: typeof Success;
+  Warning: typeof Warning;
+  Tag: typeof Tag;
 }
 
 export const Icons: ComponentProps = () => {
@@ -16,6 +40,18 @@ export const Icons: ComponentProps = () => {
 Icons.House = House;
 Icons.AppLogo = AppLogo;
 Icons.Upload = Upload;
+Icons.Add = Add;
+Icons.Delete = Delete;
+Icons.Edit = Edit;
+Icons.Close = Close;
+Icons.Refresh = Refresh;
+Icons.Search = Search;
+Icons.Arrow = Arrow;
+Icons.Chevron = Chevron;
+Icons.Error = Error;
+Icons.Success = Success;
+Icons.Warning = Warning;
+Icons.Tag = Tag;
 
 Icons.displayName = 'Icons';
 
---
+++ components/atoms/icons/library/error.tsx
@@ -0,0 +1,26 @@
+import React, { SVGAttributes } from 'react';
+
+export type Props = SVGAttributes<SVGElement>;
+
+export const Error: React.FC<Props> = ({
+  fill = 'currentColor',
+  ...props
+}: Props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-5 w-5"
+    viewBox="0 0 20 20"
+    fill={fill}
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+Error.displayName = 'Error';
+
+export default Error;
